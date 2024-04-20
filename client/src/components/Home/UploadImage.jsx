3515ffe90a3c3735a2a64058ffcc0de94d031536
rahul more <rahulmore@rahuls-MacBook-Air.local>
2024-04-20 11:04:00 +0530

    styling done

--- client/src/components/Home/UploadImage.jsx
+++ client/src/components/Home/UploadImage.jsx
@@ -1,71 +1,101 @@
 import { useState } from "react";
 import { useWeb3Context } from "../../contexts/web3Provider/useContext";
 import axios from "axios";
-
 import { validateFile } from "../../utils/fileValidation";
-
+import { ImageUp } from "lucide-react";
+import { toast } from "react-hot-toast";
 const UploadImage = ({ reloadEffect }) => {
-    const [file, setFile] = useState("");
-    const [loading, setLoading] = useState(false); // State to manage loading status
-    const { web3State } = useWeb3Context();
-    const { contractInstance, selectedAccount } = web3State;
+  const [file, setFile] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { web3State } = useWeb3Context();
+  const { contractInstance, selectedAccount } = web3State;
 
-    const uploadFile = async () => {
-        try {
-            validateFile(file);
-            const formData = new FormData();
-            formData.append("file", file);
-            const token = localStorage.getItem("token");
-            const config = {
-                headers: {
-                    "x-access-token": token,
-                },
-            };
-            const res = await axios.post(
-                `http://localhost:3000/api/uploadImage`,
-                formData,
-                config
-            );
-            if (res.data.message !== "Upload Successful!!!") {
-                throw new Error(res.data.message);
-            }
-            return res.data.ipfsHash;
-        } catch (error) {
-            console.error("Error Uploading File:", error.message);
-            throw error; // Rethrow error to be caught by the caller
-        }
-    };
+  const uploadFile = async () => {
+    try {
+      validateFile(file);
+      const formData = new FormData();
+      formData.append("file", file);
+      const token = localStorage.getItem("token");
+      const config = {
+        headers: {
+          "x-access-token": token,
+        },
+      };
+      const res = await axios.post(
+        "http://localhost:3000/api/uploadImage",
+        formData,
+        config
+      );
+      if (res.data.message !== "Upload Successful!!!") {
+        throw new Error(res.data.message);
+      }
+      return res.data.ipfsHash;
+    } catch (error) {
+      console.error("Error Uploading File:", error.message);
+      throw error; // Rethrow error to be caught by the caller
+    }
+  };
 
-    const handleImageUpload = async (e) => {
-        e.preventDefault();
-        setLoading(true); // Start loading
-        try {
-            const ipfsHash = await uploadFile();
-            console.log(ipfsHash);
-            const tx = await contractInstance.uploadFile(selectedAccount, ipfsHash);
-            await tx.wait();
-            reloadEffect();
-        } catch (error) {
-            console.error(error);
-            alert("Error Upload File: " + error.message);
-        } finally {
-            setLoading(false); // Stop loading
-        }
-    };
+  const handleImageUpload = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    try {
+      const myPromise = async () => {
+        const ipfsHash = await uploadFile();
+        console.log(ipfsHash);
+        const tx = await contractInstance.uploadFile(selectedAccount, ipfsHash);
+        await tx.wait();
+      };
+      await toast.promise(myPromise(), {
+        loading: "Uploading",
+        success: "File Uploaded Successfully",
+        error: (error) => {
+          console.error(error);
+          return "Error uploading file: " + error.message;
+        },
+      });
 
-    return (
-        <div>
-            <input
-                type="file"
-                accept=".jpg, .jpeg, .png"
-                onChange={(e) => setFile(e.target.files[0])}
-            />
-            <button onClick={handleImageUpload} disabled={!file || loading}>
-                {loading ? "Uploading..." : "Upload"}
-            </button>
-            <br />
-        </div>
-    );
+      reloadEffect();
+    } catch (error) {
+      console.error(error);
+      alert("Error Upload File: " + error.message);
+      toast.error("Error uploading file: " + error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="h-full w-screen flex flex-col justify-center items-center gap-6">
+      <p className="font-semibold md:text-[24px]">
+        Upload file with Web3's Security
+      </p>
+      <div className="w-full flex justify-center items-center">
+        <input
+          type="file"
+          accept=".jpg, .jpeg, .png"
+          onChange={(e) => setFile(e.target.files[0])}
+          className="w-[200px] md:w-[210px]"
+        />
+      </div>
+      {file ? (
+        <button
+          onClick={handleImageUpload}
+          disabled={!file || loading}
+          className="border-sky-400 border-dotted p-2 border-2 rounded-md flex flex-col justify-center items-center hover:bg-sky-200"
+        >
+          <ImageUp />
+          {loading ? "Uploading..." : "Upload"}
+        </button>
+      ) : (
+        <p className="text-[20px] font-semibold text-red-500">
+          First Please Choose a file to upload
+        </p>
+      )}
+
+      <br />
+    </div>
+  );
 };
 
 export default UploadImage;
--- client/src/components/Navbar/ConnectedAccount.jsx
+++ client/src/components/Navbar/ConnectedAccount.jsx
@@ -1,12 +1,16 @@
 import { useWeb3Context } from "../../contexts/web3Provider/useContext";
 
 const ConnectedAccount = () => {
-    const {web3State}=useWeb3Context()
-    const {selectedAccount}=web3State;
-    return ( 
-    <div>
-       <p>Connected Account: {selectedAccount}</p>
-    </div> );
-}
- 
+  const { web3State } = useWeb3Context();
+  const { selectedAccount } = web3State;
+  return (
+    <div className="w-full flex flex-col justify-center items-center md:flex-row md:justify-between md:px-10">
+      <span className="font-semibold">Connected Account : </span>
+      <p className="text-[14px] overflow-hidden whitespace-nowrap max-w-[200px] overflow-ellipsis md:max-w-full">
+        {selectedAccount}
+      </p>
+    </div>
+  );
+};
+
 export default ConnectedAccount;
